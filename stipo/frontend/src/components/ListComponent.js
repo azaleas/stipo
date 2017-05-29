70b6d4eec4dac3c789fe710da64cf8185aa093fe
azaleas <azaleas@2die4.com>
2017-05-29 21:53:21 +0400

    added filter for 5 a.m. update and current user going check

--- stipo/frontend/src/components/ListComponent.js
+++ stipo/frontend/src/components/ListComponent.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const moment = require('moment');
+
 import {api} from './../utils/Api';
 
 import {List, ListItem} from 'material-ui/List';
@@ -8,8 +10,28 @@ import Avatar from 'material-ui/Avatar';
 import Toggle from 'material-ui/Toggle';
 
 const ListComponent = (props) => {
+    let user_id;
+    /*get only attends until 5 a.m. (convert the time to utc) */
+    let currentTime = moment();
+    let updateTime = moment().set({
+        'hour': 5, 
+        'minute': 0,
+        'second': 0,
+        'millisecond': 0,
+    });
+    if (currentTime < updateTime){
+        updateTime = moment().subtract(1, 'days').set({
+            'hour': 5, 
+            'minute': 0,
+            'second': 0,
+            'millisecond': 0,
+        });
+    }
+    if (api.isLoggedIn()){
+        user_id = api.getUserId();
+    }
+    updateTime = moment.utc(updateTime);
     console.log(props.data);
-    console.log(api.isLoggedIn());
     return (
         <div className="search-results">
             <p>Search returns 20 results</p>
@@ -17,96 +39,123 @@ const ListComponent = (props) => {
                 {
                     !api.isLoggedIn()
                     ?(
-                        props.data.map((el, index) => (
-                            <ListItem
-                                key={el.id}
-                                leftAvatar={<Avatar src={el.image_url} />}
-                                primaryText={
-                                    <p className="facility-info--title">
-                                        {el.name} | <a href={el.url} target="_blank">Yelp Link</a>
-                                    </p>
-                                }
-                                secondaryTextLines={2}
-                                disabled
-                                rightToggle={
-                                    <Toggle
-                                        thumbStyle={{
-                                            backgroundColor: "rgb(245, 216, 162)",
-                                        }}
-                                        thumbSwitchedStyle={{
-                                            backgroundColor: "orange",
-                                        }}
-                                        trackStyle={{
-                                            backgroundColor: "rgba(255, 165, 0, 0.5)",
-                                        }}
-                                        trackSwitchedStyle={{
-                                            backgroundColor: "rgb(245, 216, 162)",
-                                        }}
-                                        onToggle={props.toggleAttend}
-                                     />
-                                }
-                                className="facility-infoblock"
-                                secondaryText={
-                                    <p className="facility-info">
-                                        Total going: 
-                                        <span className="facility-info--stat">
-                                            {el.attends.length}
-                                        </span>
-                                        <br/>
-                                        Rating: 
-                                        <span className="facility-info--stat">
-                                            {el.rating}
-                                        </span>
-                                    </p>
-                                }
-                            />
-                        ))
+                        props.data.map((el, index) => {
+                            let totalGoing = 0;
+                            if (el.attends.length !== 0){
+                                el.attends.forEach((el, index) => {
+                                    let attendTime = new Date(el.time);
+                                    if (attendTime >= updateTime){
+                                        totalGoing++;
+                                    }
+                                });
+                            }
+                            return (
+                                <ListItem
+                                    key={el.id}
+                                    leftAvatar={<Avatar src={el.image_url} />}
+                                    primaryText={
+                                        <p className="facility-info--title">
+                                            {el.name} | <a href={el.url} target="_blank">Yelp Link</a>
+                                        </p>
+                                    }
+                                    secondaryTextLines={2}
+                                    disabled
+                                    rightToggle={
+                                        <Toggle
+                                            thumbStyle={{
+                                                backgroundColor: "rgb(245, 216, 162)",
+                                            }}
+                                            thumbSwitchedStyle={{
+                                                backgroundColor: "orange",
+                                            }}
+                                            trackStyle={{
+                                                backgroundColor: "rgba(255, 165, 0, 0.5)",
+                                            }}
+                                            trackSwitchedStyle={{
+                                                backgroundColor: "rgb(245, 216, 162)",
+                                            }}
+                                            onToggle={props.toggleAttend}
+                                         />
+                                    }
+                                    className="facility-infoblock"
+                                    secondaryText={
+                                        <p className="facility-info">
+                                            Total going: 
+                                            <span className="facility-info--stat">
+                                                {totalGoing}
+                                            </span>
+                                            <br/>
+                                            Rating: 
+                                            <span className="facility-info--stat">
+                                                {el.rating}
+                                            </span>
+                                        </p>
+                                    }
+                                />
+                            )
+                        })
                     )
                     :(
-                        props.data.map((el, index) => (
-                            <ListItem
-                                key={el.id}
-                                leftAvatar={<Avatar src={el.image_url} />}
-                                primaryText={
-                                    <p className="facility-info--title">
-                                        {el.name} | <a href={el.url} target="_blank">Yelp Link</a>
-                                    </p>
-                                }
-                                secondaryTextLines={2}
-                                disabled
-                                rightToggle={
-                                    <Toggle
-                                        thumbStyle={{
-                                            backgroundColor: "rgb(245, 216, 162)",
-                                        }}
-                                        thumbSwitchedStyle={{
-                                            backgroundColor: "orange",
-                                        }}
-                                        trackStyle={{
-                                            backgroundColor: "rgba(255, 165, 0, 0.5)",
-                                        }}
-                                        trackSwitchedStyle={{
-                                            backgroundColor: "rgb(245, 216, 162)",
-                                        }}
-                                        onToggle={props.toggleAttend}
-                                     />
-                                }
-                                className="facility-infoblock"
-                                secondaryText={
-                                    <p className="facility-info">
-                                        Total going: 
-                                        <span className="facility-info--stat">
-                                            {el.attends.length}
-                                        </span>
-                                        <br/>
-                                        Rating: 
-                                        <span className="facility-info--stat">
-                                            {el.rating}
-                                        </span>
-                                    </p>
-                                }
-                            />
-                        ))
+                        props.data.map((el, index) => {
+                            let totalGoing = 0;
+                            let userGoing = false;
+                            if (el.attends.length !== 0){
+                                el.attends.forEach((el, index) => {
+                                    let attendTime = new Date(el.time);
+                                    if (attendTime >= updateTime && el.is_going === true){
+                                        totalGoing++;
+                                    }
+                                    if (parseInt(user_id) === parseInt(el.user) && el.is_going === true){
+                                        userGoing = true;
+                                    }
+                                });
+                            }
+                            return(
+                                <ListItem
+                                    key={el.id}
+                                    leftAvatar={<Avatar src={el.image_url} />}
+                                    primaryText={
+                                        <p className="facility-info--title">
+                                            {el.name} | <a href={el.url} target="_blank">Yelp Link</a>
+                                        </p>
+                                    }
+                                    secondaryTextLines={2}
+                                    disabled
+                                    rightToggle={
+                                        <Toggle
+                                            thumbStyle={{
+                                                backgroundColor: "rgb(245, 216, 162)",
+                                            }}
+                                            thumbSwitchedStyle={{
+                                                backgroundColor: "orange",
+                                            }}
+                                            trackStyle={{
+                                                backgroundColor: "rgba(255, 165, 0, 0.5)",
+                                            }}
+                                            trackSwitchedStyle={{
+                                                backgroundColor: "rgb(245, 216, 162)",
+                                            }}
+                                            toggled={userGoing ? true : false}
+                                            onToggle={props.toggleAttend}
+                                         />
+                                    }
+                                    className="facility-infoblock"
+                                    secondaryText={
+                                        <p className="facility-info">
+                                            Total going: 
+                                            <span className="facility-info--stat">
+                                                {totalGoing}
+                                            </span>
+                                            <br/>
+                                            Rating: 
+                                            <span className="facility-info--stat">
+                                                {el.rating}
+                                            </span>
+                                        </p>
+                                    }
+                                />
+                            )
+                        })
                     )
                 }
             </List>
