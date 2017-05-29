5d68fbce0ca4ab14f33d98bdfb54bfd92b3ffd39
azaleas <azaleas@2die4.com>
2017-05-29 16:31:23 +0400

    added toggle item

--- stipo/frontend/src/components/ListComponent.js
+++ stipo/frontend/src/components/ListComponent.js
@@ -1,42 +1,67 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import {api} from './../utils/Api';
+
 import {List, ListItem} from 'material-ui/List';
 import Avatar from 'material-ui/Avatar';
-import Divider from 'material-ui/Divider';
+import Toggle from 'material-ui/Toggle';
 
 const ListComponent = (props) => {
     return (
         <div className="search-results">
+            <p>Search returns 20 results</p>
             <List>
                 {
-                    props.data.map((el, index) => (
-                        <ListItem
-                            key={el.id}
-                            leftAvatar={<Avatar src={el.image_url} />}
-                            primaryText={
-                                <p className="facility-info--title">
-                                    {el.name} | <a href={el.url} target="_blank">Yelp Link</a>
-                                </p>
-                            }
-                            secondaryTextLines={2}
-                            disabled
-                            className="facility-infoblock"
-                            secondaryText={
-                                <p className="facility-info">
-                                    Total going: 
-                                    <span className="facility-info--stat">
-                                        {el.attends.length}
-                                    </span>
-                                    <br/>
-                                    Rating: 
-                                    <span className="facility-info--stat">
-                                        {el.rating}
-                                    </span>
-                                </p>
-                            }
-                        />
-                    ))
+                    !api.isLoggedIn()
+                    ?(
+                        props.data.map((el, index) => (
+                            <ListItem
+                                key={el.id}
+                                leftAvatar={<Avatar src={el.image_url} />}
+                                primaryText={
+                                    <p className="facility-info--title">
+                                        {el.name} | <a href={el.url} target="_blank">Yelp Link</a>
+                                    </p>
+                                }
+                                secondaryTextLines={2}
+                                disabled
+                                rightToggle={
+                                    <Toggle
+                                        thumbStyle={{
+                                            backgroundColor: "rgb(245, 216, 162)",
+                                        }}
+                                        thumbSwitchedStyle={{
+                                            backgroundColor: "orange",
+                                        }}
+                                        trackStyle={{
+                                            backgroundColor: "rgba(255, 165, 0, 0.5)",
+                                        }}
+                                        trackSwitchedStyle={{
+                                            backgroundColor: "rgb(245, 216, 162)",
+                                        }}
+                                     />
+                                }
+                                className="facility-infoblock"
+                                secondaryText={
+                                    <p className="facility-info">
+                                        Total going: 
+                                        <span className="facility-info--stat">
+                                            {el.attends.length}
+                                        </span>
+                                        <br/>
+                                        Rating: 
+                                        <span className="facility-info--stat">
+                                            {el.rating}
+                                        </span>
+                                    </p>
+                                }
+                            />
+                        ))
+                    )
+                    :(
+                        <p></p>
+                    )
                 }
             </List>
         </div>
