52e921cbf42c64ea895a4f596dcc96918a238d3e
azaleas <azaleas@2die4.com>
2017-05-29 16:08:13 +0400

    added list styling for search results

--- stipo/frontend/src/components/ListComponent.js
+++ stipo/frontend/src/components/ListComponent.js
@@ -1,9 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const ListComponent = ({ className }) => {
+import {List, ListItem} from 'material-ui/List';
+import Avatar from 'material-ui/Avatar';
+import Divider from 'material-ui/Divider';
+
+const ListComponent = (props) => {
     return (
-        <div>Hello</div>
+        <div className="search-results">
+            <List>
+                {
+                    props.data.map((el, index) => (
+                        <ListItem
+                            key={el.id}
+                            leftAvatar={<Avatar src={el.image_url} />}
+                            primaryText={
+                                <p className="facility-info--title">
+                                    {el.name} | <a href={el.url} target="_blank">Yelp Link</a>
+                                </p>
+                            }
+                            secondaryTextLines={2}
+                            disabled
+                            className="facility-infoblock"
+                            secondaryText={
+                                <p className="facility-info">
+                                    Total going: 
+                                    <span className="facility-info--stat">
+                                        {el.attends.length}
+                                    </span>
+                                    <br/>
+                                    Rating: 
+                                    <span className="facility-info--stat">
+                                        {el.rating}
+                                    </span>
+                                </p>
+                            }
+                        />
+                    ))
+                }
+            </List>
+        </div>
     );
 };
 
